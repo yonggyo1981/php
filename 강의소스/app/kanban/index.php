cc94dbb53e607e599df829d888bd967f90b90f04
YONGGYO LEE <yonggyo1981@gmail.com>
2021-11-22 16:39:15 +0900

    .

--- 강의소스/app/kanban/index.php
+++ 강의소스/app/kanban/index.php
@@ -10,15 +10,44 @@ try {
 		/** 작업 추가 */
 		case "add" : 
 			$idx = $kanban->addWork($in);
+			if (!$idx) {
+				throw new Exception("작업등록 실패하였습니다.");
+			}
+			
+			$success = true;
+			$returnData = ["idx" => $idx];
 			break;
 		/** 작업 수정 */
 		case "edit" : 
 			$result = $kanban->editWork($in);
+			if (!$result) {
+				throw new Exception("작업수정 실패하였습니다.");
+			}
+			
+			$success = true;
+			$returnData = $result;
 			break;
 		/** 작업 삭제 */
 		case "delete" : 
+			if (!isset($data['idx']) || !$data['idx']) {
+					throw new Exception("작업등록번호 누락");
+			}
+			
+			$info = $kanban->get($data['idx']);
+			if (!$info) {
+				throw new Exception("삭제할 작업내역이 없습니다.");
+			}
+			
+			if ($info['memNo'] != $data['memNo']) {
+				throw new Exception("본인이 작성한 작업내역만 삭제 가능합니다.");
+			}
 			
 			$result = $kanban->deleteWork(Request::get("idx"));
+			if (!$result) {
+				throw new Exception("작업삭제 실패하였습니다.");
+			}
+			
+			$success = true;
 			break;
 		/** 작업 목록 */
 		case "getList" : 
@@ -36,7 +65,7 @@ try {
 			break;
 		default :
 			if (Request::get("origin") != 'front') {
-				
+				header("Location: /app");
 				exit;
 			}
 	}
--- 강의소스/app/member/index.php
+++ 강의소스/app/member/index.php
@@ -10,20 +10,42 @@ try {
 	switch (Request::get("mode")) {
 		case "join": // 회원가입 처리 
 			$memberInfo = $member->join($in);
+			if ($memberInfo) {
+				$success = true;
+				$returnData = $memberInfo;
+			} else {
+				throw new Exception("회원가입 실패");
+			}
 			break;
 		case "update" :  // 회원정보 수정
 			$result = $member->update($in);
+			if (!$result) {
+				throw new Exception("회원정보 수정 실패하였습니다.");
+			}
+			$success = true;
+			$message = "회원정보가 수정되었습니다.";
 			break;
 		case "login" : // 로그인 처리 
 			$token = $member->login($in);
+			if (!$token) {
+				throw new Exception("로그인 실패하였습니다.");
+			}
+			$success = true;
+			$returnData = ["token" => $token];
 			break;
 		/** 토큰으로 회원 정보 조회 */
 		case "get_member" : 
 			$memberData = $member->getByToken($in['token']);
+			if (!$memberData) {
+				throw new Exception("토큰 회원조회 실패");
+			}
+			
+			$success = true;
+			$returnData = $memberData;
 			break;
 		default : 
 			if (Request::get("origin") != 'front') {
-				//header("Location : /app");
+				header("Location: /app");
 				exit;
 			}
 	}
