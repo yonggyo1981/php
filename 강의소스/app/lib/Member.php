bc168867ca711a0804126161af79f97a4bb97e79
YONGGYO LEE <yonggyo1981@gmail.com>
2021-11-22 10:37:12 +0900

    .

--- 강의소스/app/lib/Member.php
+++ 강의소스/app/lib/Member.php
@@ -4,6 +4,18 @@
 *
 */
 class Member {
-	private $db;
+	private $db; // DB 인스턴스 
+	private static $instance;
 	
+	private function __construct() {
+		$this->db = DB::getInstance();
+	}
+	
+	public static function getInstance() {
+		if (!self::$instance) {
+			self::$instance = new Member();
+		}
+		
+		return self::$instance;
+	}
 }
