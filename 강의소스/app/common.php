795d42b4e3a49c55de418ec0c9c947d41f06c4ff
YONGGYO LEE <yonggyo1981@gmail.com>
2021-11-22 11:22:21 +0900

    .

--- 강의소스/app/common.php
+++ 강의소스/app/common.php
@@ -1,6 +1,10 @@
 <?php
 header("Access-Control-Allow-Origin: *"); // CORS
 header("Access-Control-Allow-Headers: *"); // CORS
+header("Content-Type: application/json; charset=utf-8");
 include "lib/DB.php"; // DB 클래스
 include "lib/Member.php"; // Member 클래스
 include "lib/Kanban.php"; // Kanban 클래스
+
+//$in = $_REQUEST?$_REQUEST:[];
+$in = $_REQUEST ?? [];
--- 강의소스/app/member/index.php
+++ 강의소스/app/member/index.php
@@ -4,3 +4,31 @@ include_once "../common.php";
 * 회원가입, 로그인 처리 
 *
 */
+$success = false;
+$returnData = [];
+$message = "";
+$member = Member::getInstance(); // Member 인스턴스
+try {
+	switch ($in['mode']) {
+		case "join": // 회원가입 처리 
+			$memberInfo = $member->join($in);
+			break;
+		case "update" :  // 회원정보 수정
+			$result = $member->update($in);
+			break;
+		case "login" : // 로그인 처리 
+			$token = $member->login($in);
+			break;
+		
+	}
+} catch(Exception $e) {
+	$message = $e->getMessage();
+}
+
+$result = [
+	'success' => $success,
+	'data' => $returnData,
+	'message' => $message,
+];
+
+echo json_encode($result);
